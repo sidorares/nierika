52a1b812b40d6231b7c7b600328c760a664520f7
Andrey Sidorov <sidorares@yandex.com>
2017-02-25 16:46:32 +1100

    initial test

---
+++ test/test-rfb-connect.js
@@ -0,0 +1,20 @@
+const assert = require('assert')
+
+describe('rfb client', () => {
+  it('can connect to docker test image', done => {
+    const rfb = require('rfb2')
+    const r = rfb.createConnection({
+      host: '127.0.0.1',
+      port: 5902,
+      password: 'vncpassword',
+      encodings: [rfb.encodings.raw, rfb.encodings.pseudoCursor]
+    });
+    r.on('connect', () => {
+      assert.equal(r.width, 1280)
+      assert.equal(r.height, 1024)
+      r.end()
+      done()
+    });
+    r.on('error', done)
+  });
+});
