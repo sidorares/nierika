366a9d8a656d2f701b88a57f9ab56f1c89d631d6
Andrey Sidorov <sidorares@yandex.com>
2017-03-02 08:38:25 +1100

    wip

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ wip - nothing here yet
 https://raw.githubusercontent.com/naptha/tessdata/gh-pages/3.02/eng.traineddata.gz
 
 ```
-docker run consol/centos-xfce-vnc
+docker run -it -p 5911:5901 -e "VNC_PW=my-new-password" consol/centos-xfce-vnc
 ```
--- test/test-screen.js
+++ test/test-screen.js
@@ -1,1 +1,16 @@
+const rfb = require('rfb2');
 const createScreen = require('../lib/screen.js').createScreen;
+const opts = {
+  host: '127.0.0.1',
+  port: 5911,
+  password: 'my-new-password',
+  encodings: [rfb.encodings.raw, rfb.encodings.pseudoCursor]
+};
+
+describe('screen', () => {
+  it('can find an image', async () => {
+    const screen = createScreen(opts)
+    const template = await screen.createTemplateFromFile(__dirname + '/fixtures/chrome-icon.png')
+    const match = await screen.wait(template)
+  })
+})
