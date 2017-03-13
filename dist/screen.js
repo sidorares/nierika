e03bf9a6df43bc1bbee9afc049914e95b846fb33
Andrey Sidorov <sidorares@yandex.com>
2017-03-13 11:58:17 +1100

    compile

--- dist/screen.js
+++ dist/screen.js
@@ -3,6 +3,7 @@ const cv = require('opencv');
 const util = require('util');
 const throttle = require('lodash.throttle');
 const keysym = require('keysym');
+const fs = require('fs');
 const EventEmitter = require('events').EventEmitter;
 const Template = require('./template.js')
 
@@ -22,12 +23,13 @@ function Screen(opts) {
     y: 0,
   };
   this._opts = opts; // TODO merge default opts into ots, remove lines below
-  this._mouseMoveSpeed = 1400; // pix/sec
+  this._mouseMoveSpeed = 1000; // pix/sec
   this._mouseClickSpeed = 200;
   this._mouseDoubleClickSpeed = 100;
   this._keyboardTypeSpeed = 10;
   this._minSize = 12;
-  this._minMatchInterval = 500;
+  this._minMatchInterval = 250;
+  this._cursorShape = null;
   this._rfbConn.on('connect', () => {
     const r = this._rfbConn;
     this._screenBuffer = new cv.Matrix(r.height, r.width, cv.Constants.CV_8UC3);
@@ -40,33 +42,47 @@ function Screen(opts) {
     }
     */
     this.emit('connect')
-    console.log('CONNECTED  =====')
     r.requestUpdate(true, 0, 0, r.width, r.height);
     r.on('rect', this._handleRawUpdate.bind(this))
   })
-  this._rfbConn.on('error', e => this.emit('error', e))
+  this._rfbConn.on('error', e => {
+    this._rfbError = e
+    this.emit('error', e)
+  })
 }
 util.inherits(Screen, EventEmitter);
 
 Screen.prototype._handleRawUpdate = function(rect) {
-  console.log('GOT UPDATE', rect.width, rect.height);
-  if (rect.width === 0 || rect.height === 0) {
-    return;
-  }
   const r = this._rfbConn;
+  if (rect.encoding == rfb.encodings.raw) {
+    if (rect.width === 0 || rect.height === 0) {
+      return;
+    }
+    const r = this._rfbConn;
+    const update = new cv.Matrix(rect.height, rect.width, cv.Constants.CV_8UC4);
+    update.put(rect.buffer)
+    rgb(update).copyTo(this._screenBuffer, rect.x, rect.y);
+    this.emit('screen-update', rect);
+  } else if (rect.encoding === rfb.encodings.pseudoDesktopSize) {
+    this._screenBuffer = new cv.Matrix(rect.height, rect.width, cv.Constants.CV_8UC3);
+    // TODO anything else required to handle resize?
+  } else if (rect.encoding === rfb.encodings.pseudoCursor) {
+    this._cursorShape = rect
+    this.emit('cursor-update', rect);
+  }
   r.requestUpdate(true, 0, 0, r.width, r.height);
-  const update = new cv.Matrix(rect.height, rect.width, cv.Constants.CV_8UC4);
-  update.put(rect.buffer)
-  rgb(update).copyTo(this._screenBuffer, rect.x, rect.y);
-  this.emit('screen-update', rect);
 }
 
 Screen.prototype._connected = function() {return __async(function*(){
   if (this._screenBuffer) {
     return Promise.resolve()
   }
+  if (this._rfbError) {
+    return Promise.reject(this._rfbError)
+  }
   return new Promise((accept, reject) => {
-    this.on('connect', accept)
+    this.once('connect', accept)
+    this.once('error', reject)
   })
 }.call(this))}
 
@@ -76,34 +92,30 @@ Screen.prototype.sleep = function(ms) {return __async(function*(){
   })
 }())}
 
-Screen.prototype.createTemplateFromFile = function(path) {return __async(function*(){
+ Screen.prototype.createTemplateFromFile = function(path) {return __async(function*(){
   return new Promise((accept, reject) => {
     const template = new cv.Matrix();
-    cv.readImage(path, (err, img) => {
+    // node-opencv does not error if file is not found and instead
+    // just returns 0x0 matriz :(
+    // add extra check on top
+    fs.stat(path, (err, stat) => {
       if (err) {
         return reject(err);
       }
-      img.convertTo(template, cv.Constants.CV_8UC4);
-      accept(new Template({
-        matrix: rgb(template),
-        pyramidMinSize: this._opts.minTemplateSize
-      }))
+      cv.readImage(path, (err, img) => {
+        if (err) {
+          return reject(err);
+        }
+        img.convertTo(template, cv.Constants.CV_8UC4);
+        accept(new Template({
+          matrix: rgb(template),
+          pyramidMinSize: this._opts.minTemplateSize,
+          name: path
+        }))
+      })
     })
   })
 }.call(this))}
-
-function clamp(val, max) {
-  if (val < 0) {
-    return 0;
-  }
-  if (val > max) {
-    return max;
-  }
-  return val
-}
-
-let counter = 0;
-let rect_counter = 0;
 
 Screen.prototype.waitVisible = function(template, maxWait=0) {return __async(function*(){
   yield this._connected();
@@ -112,11 +124,9 @@ Screen.prototype.waitVisible = function(template, maxWait=0) {return __async(fun
     let dirtyArea = null;
 
     const findMatch = (rect) => {
-      console.log('findMatch!', rect)
       dirtyArea = null
 
-      let region = this._screenBuffer
-      region.save(`screen-${counter}-full.png`)
+      let region = this._screenBuffer;
 
       if (rect) {
         let left  = rect.x - template.width()
@@ -138,16 +148,10 @@ Screen.prototype.waitVisible = function(template, maxWait=0) {return __async(fun
         region = region.roi(left, top, right - left, bottom - top)
       }
 
-      counter++;
-
-      region.save(`screen-${counter}-${rect_counter}.png`)
-      template._mat.save(`screen-${counter}-template.png`)
-
       const origin = region.locateROI();
       const res = region.matchTemplateByMatrix(template._mat);
       const m = res.templateMatches(template.similarity(), 1, 1)
       if (m[0]) {
-        console.log('Got match!', m, ' count:', counter)
         this.removeListener('screen-update', handleScreenUpdate)
         if (timeout) {
           clearTimeout(timeout);
@@ -206,7 +210,39 @@ Screen.prototype.waitVisible = function(template, maxWait=0) {return __async(fun
 
 Screen.prototype.waitVanish = function(template) {return __async(function*(){
   yield this._connected()
+
+  return new Promise((accept, reject) => {
+    // v1, super non-optimized.
+    let throttledCheckMatches = null;
+    const checkMatches = () => {
+      const maxMatches = 1;
+      const region = this._screenBuffer;
+      const res = region.matchTemplateByMatrix(template._mat);
+      const m = res.templateMatches(template.similarity(), 1, maxMatches)
+      if (!m[0]) {
+        this.removeListener('screen-update', throttledCheckMatches)
+        accept()
+      }
+    }
+    throttledCheckMatches = throttle(checkMatches, this._minMatchInterval);
+    checkMatches()
+    this.on('screen-update', throttledCheckMatches);
+  });
+
   // ...
+  // pseudo-code
+  // 0) initial region = screen
+  // 1) create a list of all matches visible on initial region right now (make sure it's converted from raw matches list to list of non-intersection regions. Or maibe "not-too-intersecting?")
+  //  - if list is empty, done.
+  // on screen updates, maintain 'dirty region'
+  // if update touches area in the current list, re-check if area still contains match. If not, remove from list
+  // once list is empty - go to step 1 using 'dirty area' for initial region
+
+  // not-too-intersecting: overlap area < some factor from main rect area
+  // http://jsfiddle.net/uthyZ/
+  // x_overlap = Math.max(0, Math.min(x12,x22) - Math.max(x11,x21))
+  // y_overlap = Math.max(0, Math.min(y12,y22) - Math.max(y11,y21));
+  // area = x_overlap*y_overlap
 }.call(this))}
 
 Screen.prototype.exists = function(template) {return __async(function*(){
@@ -220,18 +256,52 @@ function sleep(ms) {
   })
 }
 
-Screen.prototype.mouseLeftClick = function() {return __async(function*(){
+Screen.prototype.mouseButtonDown = function(button) {return __async(function*(){
+  yield this._connected()
+  const buttonMask = 1 << button;
+  this._mouseState.buttons = this._mouseState.buttons | buttonMask;
   this._rfbConn.pointerEvent(
     this._mouseState.x,
     this._mouseState.y,
-    1//this._mouseState.buttons | 1
+    this._mouseState.buttons
   );
+}.call(this))}
+
+Screen.prototype.mouseButtonUp = function(button) {return __async(function*(){
+  yield this._connected()
+  const buttonMask = 1 << button;
+  this._mouseState.buttons = this._mouseState.buttons & (~buttonMask)
+  this._rfbConn.pointerEvent(
+    this._mouseState.x,
+    this._mouseState.y,
+    this._mouseState.buttons
+  );
+}.call(this))}
+
+Screen.prototype.mouseButtonClick = function(button) {return __async(function*(){
+  yield this.mouseButtonDown(button)
   yield sleep(this._mouseClickSpeed);
-  this._rfbConn.pointerEvent(
-    this._mouseState.x,
-    this._mouseState.y,
-    0//this._mouseState.buttons & 0xfe
-  );
+  this.mouseButtonUp(button)
+}.call(this))}
+
+Screen.prototype.mouseLeftClick = function() {return __async(function*(){
+  return this.mouseButtonClick(0)
+}.call(this))}
+
+Screen.prototype.mouseMiddleClick = function() {return __async(function*(){
+  return this.mouseButtonClick(1)
+}.call(this))}
+
+Screen.prototype.mouseRightClick = function() {return __async(function*(){
+  return this.mouseButtonClick(2)
+}.call(this))}
+
+Screen.prototype.mouseWheeDown = function() {return __async(function*(){
+  return this.mouseButtonClick(3)
+}.call(this))}
+
+Screen.prototype.mouseWheelUp = function() {return __async(function*(){
+  return this.mouseButtonClick(4)
 }.call(this))}
 
 Screen.prototype.mouseLeftDoubleClick = function() {return __async(function*(){
@@ -241,6 +311,11 @@ Screen.prototype.mouseLeftDoubleClick = function() {return __async(function*(){
 }.call(this))}
 
 Screen.prototype.mouseMove = function(x, y) {return __async(function*(){
+  if (x.constructor === Template) {
+    const match = yield this.waitVisible(x)
+    return this.mouseMove(match.x, match.y)
+  }
+
   const location = {x, y};
   yield this._connected()
 
@@ -248,7 +323,7 @@ Screen.prototype.mouseMove = function(x, y) {return __async(function*(){
   const dy = location.y - this._mouseState.y;
   const d = Math.sqrt(dx*dx + dy*dy);
 
-  const eta = 1000 *d / this._mouseMoveSpeed;
+  const eta = 1000 * d / this._mouseMoveSpeed;
 
   const startTime = +new Date()
   const startX = this._mouseState.x
@@ -289,7 +364,6 @@ Screen.prototype.keyboardKeyUp = function(key) {return __async(function*(){
 }.call(this))};
 
 Screen.prototype.keyboardKeyPress = function(key) {return __async(function*(){
-  yield this._connected();
   yield this.keyboardKeyDown(key);
   yield sleep(this._keyboardTypeSpeed);
   yield this.keyboardKeyUp(key);
